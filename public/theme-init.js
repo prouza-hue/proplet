a79804e03c4d4c3b9bc3cbad595591b95dd4b0a2
prouza-hue <prouza@gmail.com>
2026-08-15 12:18:45 +0200

    Bust home layout cache after novice icon tweak

--- public/theme-init.js
+++ public/theme-init.js
@@ -24,13 +24,13 @@
 
   const css=document.createElement('link');
   css.rel='stylesheet';
-  css.href='/home-layout.css?v=7';
+  css.href='/home-layout.css?v=8';
   document.head.appendChild(css);
 
   const loadHomeLayout=()=>{
     if(document.querySelector('script[data-proplet-home-layout]'))return;
     const script=document.createElement('script');
-    script.src='/home-layout.js?v=7';
+    script.src='/home-layout.js?v=8';
     script.dataset.propletHomeLayout='1';
     document.body.appendChild(script);
   };
