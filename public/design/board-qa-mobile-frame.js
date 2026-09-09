b2a369c74312e138e54c1fc83eba1db9befd0853
prouza-hue <prouza@gmail.com>
2026-09-09 14:20:50 +0200

    Remove temporary preview board QA routes

--- public/design/board-qa-mobile-frame.js
+++
@@ -1,7 +0,0 @@
-(function loadMobileBoardQa(){
-'use strict';
-const params=new URLSearchParams(location.search);
-params.set('boardQaState',params.get('boardQaState')||'default');
-params.set('boardQaDifficulty',params.get('boardQaDifficulty')||'easy');
-fetch('/?'+params.toString()).then(response=>response.text()).then(html=>{document.querySelector('#qa').srcdoc=html});
-})();
