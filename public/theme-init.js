8b03bf235b1e5dcbd6561f81324097446b193377
prouza-hue <prouza@gmail.com>
2026-09-11 17:25:18 +0200

    Load updated game release polish

--- public/theme-init.js
+++ public/theme-init.js
@@ -62,7 +62,7 @@
     ['/app-profile-settings.css?v=40140-s13b','propletAppProfileSettingsCss'],
     ['/typography-readability-v1.css?v=1','propletTypographyReadabilityV1Css'],
     ['/tajenka-release-fix.css?v=1','propletTajenkaReleaseFixCss'],
-    ['/game-release-polish.css?v=1','propletGameReleasePolishCss'],
+    ['/game-release-polish.css?v=2','propletGameReleasePolishCss'],
   ];
 
   const loadStyle=(href,key)=>{
